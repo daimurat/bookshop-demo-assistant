4d66b7c6d234857ed4484229a692ec3b1c9b2ae0
daimurat <d.murata0623@gmail.com>
2025-05-21 13:19:26 +0900

    added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import assistant
 from ingest import ingest_data
 
-ingest_data()
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(assistant.router)
 
